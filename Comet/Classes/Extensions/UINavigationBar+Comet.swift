f8cf1020ee8cbd7d2c985e645a6a1fd3b4797094
Harley <harley.gb@foxmail.com>
2016-11-21 10:01:47 +0800

    更新导航栏设置API，遵循 Swift 3.0 API 风格

--- Comet/Classes/Extensions/UINavigationBar+Comet.swift
+++ Comet/Classes/Extensions/UINavigationBar+Comet.swift
@@ -10,15 +10,35 @@ import Foundation
 
 public extension UINavigationBar {
     
-    public func setTextColor(_ color: UIColor) {
-        var attributes = self.titleTextAttributes ?? [String : Any]()
-        attributes[NSForegroundColorAttributeName] = color
-        self.titleTextAttributes = attributes
+    /// 定义导航栏的全局颜色
+    public class func customizeAppearenceColorWith(barTint: UIColor, foreground: UIColor) {
+        
+        self.appearance().barTintColor = barTint
+        self.appearance().tintColor = foreground
+        self.appearance().textColor = foreground
     }
     
-    public func setTitleFont(_ font: UIFont) {
-        var attributes = self.titleTextAttributes ?? [String : Any]()
-        attributes[NSFontAttributeName] = font
-        self.titleTextAttributes = attributes
+    /// 设置导航栏的文字颜色
+    public var textColor: UIColor? {
+        get {
+            return self.titleTextAttributes?[NSForegroundColorAttributeName] as? UIColor
+        }
+        set {
+            var attributes = self.titleTextAttributes ?? [String : Any]()
+            attributes[NSForegroundColorAttributeName] = newValue
+            self.titleTextAttributes = attributes
+        }
+    }
+    
+    /// 设置导航栏标题的字体
+    public var titleFont: UIFont? {
+        get {
+            return self.titleTextAttributes?[NSFontAttributeName] as? UIFont
+        }
+        set {
+            var attributes = self.titleTextAttributes ?? [String : Any]()
+            attributes[NSFontAttributeName] = newValue
+            self.titleTextAttributes = attributes
+        }
     }
 }
