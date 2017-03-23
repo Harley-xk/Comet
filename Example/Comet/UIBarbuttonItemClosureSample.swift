76e7c2e45b31db0f434d4f5161ae9dd19d68c5e7
Harley.xk <harley.gb@foxmail.com>
2017-03-23 17:37:21 +0800

    Add closure support to UIBarButtonItem； Add samples

---
+++ Example/Comet/UIBarbuttonItemClosureSample.swift
@@ -0,0 +1,40 @@
+//
+//  UIBarbuttonItemClosureSample.swift
+//  Comet
+//
+//  Created by Harley.xk on 2017/3/23.
+//  Copyright © 2017年 CocoaPods. All rights reserved.
+//
+
+import UIKit
+import Comet
+
+class UIBarbuttonItemClosureSample: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Action", style: .plain, callback: { (sender) in
+            let alert = UIAlertController(title: "Action!", message: nil, preferredStyle: .alert)
+            alert.addAction(title: "确定", style: .cancel)
+            self.present(alert, animated: true, completion: nil)
+        })
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
