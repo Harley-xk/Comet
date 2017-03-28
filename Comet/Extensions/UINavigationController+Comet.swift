5743230e0f382827a1726a157867c8bed8f97dd0
Harley.xk <harley.gb@foxmail.com>
2017-03-28 10:56:58 +0800

    简化 navigation 的 pop、push 方法

--- Comet/Extensions/UINavigationController+Comet.swift
+++ Comet/Extensions/UINavigationController+Comet.swift
@@ -17,4 +17,14 @@ extension UINavigationController {
         vcs.append(viewController)
         setViewControllers(vcs, animated: animated)
     }
+    
+    /// 简化 push 函数
+    open func push(_ viewController: UIViewController, animated: Bool = true) {
+        pushViewController(viewController, animated: animated)
+    }
+    
+    /// 简化 pop 函数，且不强制要求接收返回值
+    @discardableResult open func pop(animated: Bool = true) -> UIViewController? {
+        return popViewController(animated: animated)
+    }
 }
