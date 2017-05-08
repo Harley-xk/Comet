ad0790f6f7f98cf2a4ecf09cc0c30c203c251d96
Harley.xk <harley.gb@foxmail.com>
2017-05-08 09:18:04 +0800

    增加在 IB 中设置本地化文字的方法

---
+++ Comet/Extensions/Text+Localize.swift
@@ -0,0 +1,40 @@
+//
+//  Text+Localize.swift
+//  Pods
+//
+//  Created by Harley.xk on 2017/5/8.
+//
+//
+
+import Foundation
+import UIKit
+
+extension UILabel {
+    @IBInspectable var localizedKey: String? {
+        set {
+            guard let newValue = newValue else { return }
+            text = NSLocalizedString(newValue, comment: "")
+        }
+        get { return text }
+    }
+}
+
+extension UIButton {
+    @IBInspectable var localizedKey: String? {
+        set {
+            guard let newValue = newValue else { return }
+            setTitle(NSLocalizedString(newValue, comment: ""), for: .normal)
+        }
+        get { return titleLabel?.text }
+    }
+}
+
+extension UITextField {
+    @IBInspectable var localizedKey: String? {
+        set {
+            guard let newValue = newValue else { return }
+            placeholder = NSLocalizedString(newValue, comment: "")
+        }
+        get { return placeholder }
+    }
+}
