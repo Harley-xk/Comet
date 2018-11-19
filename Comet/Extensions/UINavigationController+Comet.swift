6da148292d1faea3542e052077ef0d65e6b06a7f
Harley-xk <harley.gb@foxmail.com>
2018-11-19 16:17:49 +0800

    导航控制器增加延迟出栈扩展

--- Comet/Extensions/UINavigationController+Comet.swift
+++ Comet/Extensions/UINavigationController+Comet.swift
@@ -24,9 +24,22 @@ extension UINavigationController {
     }
     
     /// 简化 pop 函数，且不强制要求接收返回值
+    ///
+    /// - Parameters:
+    ///   - delay: 延迟出栈，用于某些需要先提示用户状态然后退出界面的场景，传 0 则立刻执行出栈动作
+    ///   - animated: 是否显示出栈动画
+    /// - Returns: 被出栈的视图控制器, 使用延迟出栈时固定返回 nil
     @discardableResult
-    open func pop(animated: Bool = true) -> UIViewController? {
-        let poped = popViewController(animated: animated)
+    open func pop(delay: TimeInterval = 0, animated: Bool = true) -> UIViewController? {
+        var poped: UIViewController? = nil
+        let popAction = {
+            poped = self.popViewController(animated: animated)
+        }
+        if delay > 0 {
+            DispatchQueue.main.asyncAfter(delay: delay, execute: popAction)
+        } else {
+            popAction()
+        }
         return poped
     }
 }
