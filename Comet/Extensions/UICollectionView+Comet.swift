44d2fe271202b4d25dde8caae2bf75d5901011da
Harley <harley.gb@foxmail.com>
2019-10-14 15:42:00 +0800

    UICollectionView 添加一个简便注册 Cell 的方法

--- Comet/Extensions/UICollectionView+Comet.swift
+++ Comet/Extensions/UICollectionView+Comet.swift
@@ -10,6 +10,12 @@ import UIKit
 
 extension UICollectionView {
     
+    /// 快速注册可服用的 cell，使用类型名称作为 ReuseIdentifier
+    public func register<Cell: UICollectionViewCell>(cell: Cell.Type) {
+        let identifier = String(describing: Cell.self)
+        register(cell, forCellWithReuseIdentifier: identifier)
+    }
+    
     /// 快速创建可重用的 cell，使用 cell 的类名作为 ReuseIdentifier
     public func dequeueReusableCell<Cell: UICollectionViewCell>(for indexPath: IndexPath) -> Cell {
         let identifier = String(describing: Cell.self)
