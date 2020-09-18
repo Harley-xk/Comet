f3aca099b592cda65f78512ab4ad2d027ff74706
Harley <harley.gb@foxmail.com>
2020-09-18 14:18:41 +0800

    CollectionView Extension - 增加 SupplementaryView 的快速注册和复用方法

--- Comet/Extensions/UICollectionView+Comet.swift
+++ Comet/Extensions/UICollectionView+Comet.swift
@@ -10,7 +10,7 @@ import UIKit
 
 extension UICollectionView {
     
-    /// 快速注册可服用的 cell，使用类型名称作为 ReuseIdentifier
+    /// 快速注册可复用的 cell，使用类型名称作为 ReuseIdentifier
     public func register<Cell: UICollectionViewCell>(cell: Cell.Type) {
         let identifier = String(describing: Cell.self)
         register(cell, forCellWithReuseIdentifier: identifier)
@@ -20,5 +20,17 @@ extension UICollectionView {
     public func dequeueReusableCell<Cell: UICollectionViewCell>(for indexPath: IndexPath) -> Cell {
         let identifier = String(describing: Cell.self)
         return dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath) as! Cell
+    }
+    
+    /// 快速注册可复用的 SupplementaryView，使用类型名称作为 ReuseIdentifier
+    public func registerSupplementaryView<V: UIView>(_ type: V.Type = V.self, ofKind kind: String) {
+        let identifier = String(describing: V.self)
+        register(V.self, forSupplementaryViewOfKind: kind, withReuseIdentifier: identifier)
+    }
+    
+    /// 快速获取可复用的 SupplementaryView，使用类型名称作为 ReuseIdentifier
+    public func dequeueSupplementaryView<V: UIView>(_ type: V.Type = V.self, ofKind kind: String, for indexPath: IndexPath) -> V {
+        let identifier = String(describing: V.self)
+        return dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: identifier, for: indexPath) as! V
     }
 }
